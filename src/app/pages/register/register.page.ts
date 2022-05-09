b5dc0449e1eb2e31844867207245869524f960e4
Lana Koshelieva <lana.koshelieva@gmail.com>
2022-05-09 17:15:32 +0200

    aggiunto ReactiveForms

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  
+  registerForm;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) 
+  {
+    this.registerForm = this.fb.group 
+    ({
+      nome: ['', Validators.required],
+      username: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.pattern, Validators.minLength(8)]],
+    })
+  }
 
   ngOnInit() {
   }
